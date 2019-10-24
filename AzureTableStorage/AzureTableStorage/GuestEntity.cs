e1686677a1f979ef70f10a9486de9a7d75833d2a
Ajit <ajit.sundaresh@gmail.com>
2019-10-24 12:42:37 +0530

    Commit1

---
+++ AzureTableStorage/AzureTableStorage/GuestEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTableStorage
+{
+    class GuestEntity : TableEntity
+    {
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+
+        public GuestEntity()
+        {
+
+        }
+        public GuestEntity(string PartitionKey,string RowKey)
+        {
+            this.PartitionKey = PartitionKey;
+            this.RowKey = RowKey;
+        }
+           
+    }
+}
